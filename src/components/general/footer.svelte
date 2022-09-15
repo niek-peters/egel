745377c82ec0de381d6b2ff57d18bf5533cc9510
MrValk <n.s.peters05@gmail.com>
2022-09-15 20:54:49 +0200

    Added a basic footer

---
+++ src/components/general/footer.svelte
@@ -0,0 +1,19 @@
+<div class="relative w-full flex justify-center">
+	<div class="bg absolute bottom-0 w-full -z-10" />
+	<footer class="flex w-1/3 justify-center mb-12 z-10 text-white">
+		<h3>
+			Copyright &copy; | <a
+				class="border-b hover:text-gray-300 hover:border-gray-300 transition"
+				href="https://github.com/Egel-Developers"
+				target="_blank">Egel Developers</a
+			> 2022
+		</h3>
+	</footer>
+</div>
+
+<style>
+	.bg {
+		height: 48rem;
+		background-image: -webkit-linear-gradient(95deg, rgb(168 85 247) 50%, white 50.3%);
+	}
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,9 +2,11 @@
 	import '../app.scss';
 
 	import Header from '../components/general/header.svelte';
+	import Footer from '../components/general/footer.svelte';
 </script>
 
 <div class="w-full h-full flex flex-col items-center">
 	<Header />
 	<slot />
+	<Footer />
 </div>
