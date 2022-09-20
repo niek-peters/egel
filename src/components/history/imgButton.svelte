ebdbfc7e168586a5d42868a7dd4eb4f30b8dda72
MrValk <n.s.peters05@gmail.com>
2022-09-20 18:45:22 +0200

    Added a section on the history page where visitors can send in their own entries for the timeline

---
+++ src/components/history/imgButton.svelte
@@ -0,0 +1,87 @@
+<script lang="ts">
+	import { browser } from '$app/environment';
+	import Fa from 'svelte-fa';
+	import { faPlus } from '@fortawesome/free-solid-svg-icons';
+
+	// @ts-ignore
+	import cropImage from '../../scripts/cropImage';
+
+	async function updateImgPreview() {
+		if (!browser) return;
+		if (!imgInput.files || !imgInput.files[0]) return;
+
+		const reader = new FileReader();
+
+		reader.readAsDataURL(imgInput.files[0]);
+
+		reader.onload = function (event) {
+			const imgElement = document.createElement('img');
+			if (!imgElement || !event.target || !event.target.result) return;
+
+			imgElement.src = event.target.result as string;
+
+			imgElement.onload = function (e) {
+				const img = e.target as CanvasImageSource;
+				if (
+					!e.target ||
+					!img.width ||
+					!img.height ||
+					typeof img.width !== 'number' ||
+					typeof img.height !== 'number'
+				)
+					return;
+
+				const canvas = document.createElement('canvas');
+				canvas.width = 384;
+				canvas.height = 216;
+
+				const ctx = canvas.getContext('2d');
+				if (!ctx) return;
+
+				cropImage(ctx, img, 0, 0, canvas.width, canvas.height);
+
+				imgUrl = ctx.canvas.toDataURL('image/jpg');
+			};
+		};
+	}
+
+	export let imgUrl: string = '';
+	let imgInput: HTMLInputElement;
+</script>
+
+<div class="h-full">
+	<label for="image" class="block text-lg font-semibold mb-2">Afbeelding</label>
+	<button
+		id="image"
+		type="button"
+		class={`img-button flex outline-none items-center justify-center relative rounded-lg bg-purple-400 ${
+			imgUrl ? '' : 'hover:bg-purple-500'
+		} transition mb-2`}
+		on:click={() => imgInput.click()}
+	>
+		<input
+			class="hidden"
+			type="file"
+			accept=".jpg, .jpeg, .png"
+			bind:this={imgInput}
+			on:change={updateImgPreview}
+		/>
+		{#if imgUrl}
+			<img src={imgUrl} alt="card img" class="rounded-lg overflow-hidden object-cover" />
+		{:else}
+			<Fa icon={faPlus} class="text-7xl" />
+		{/if}
+	</button>
+</div>
+
+<style lang="scss">
+	button {
+		width: 42.67rem;
+		height: 24rem;
+
+		img {
+			width: inherit;
+			height: inherit;
+		}
+	}
+</style>
--- src/routes/(non-home)/geschiedenis/+page.svelte
+++ src/routes/(non-home)/geschiedenis/+page.svelte
@@ -5,11 +5,15 @@
 	import Line from '../../../components/general/line.svelte';
 	import Entry from '../../../components/history/entry.svelte';
 	import TeamCard from '../../../components/general/teamCard.svelte';
+
+	import ImgButton from '../../../components/history/imgButton.svelte';
 </script>
 
 <main class="flex flex-col items-center w-full">
-	<div class="relative flex flex-col items-center w-full">
-		<article class="relative flex justify-between mt-28 mb-12 2xl:w-3/5 sm:w-4/5">
+	<div class="flex flex-col items-center w-full">
+		<article
+			class="absolute flex justify-between 2xl:w-3/5 sm:w-4/5 top-1/2 left-1/2 -translate-x-1/2 -translate-y-3/4"
+		>
 			<div class="w-4/5">
 				<h1 class="text-7xl font-semibold mb-8">De geschiedenis van Egel</h1>
 				<p class="text-2xl">
@@ -22,28 +26,83 @@
 			</div>
 			<Fa
 				style="font-size: 24rem;"
-				class="absolute left-2/3 text-purple-500/20 rotate-12"
+				class="absolute left-3/4 text-purple-500/20 rotate-12"
 				icon={faHourglass}
 			/>
 		</article>
 	</div>
-	<div style="margin-bottom: 22vh;" />
+	<div style="margin-bottom: 84vh;" />
 	<Line />
-	<div class="2xl:w-3/5 sm:w-4/5">
-		<h2 class="text-4xl font-semibold mb-4 border-b border-gray-300 pb-3">Tijdlijn</h2>
-	</div>
-	<div
-		class="timeline overflow-y-auto flex flex-col justify-between my-12 2xl:w-3/5 sm:w-4/5 bg-gray-100 rounded-lg shadow-md"
-	>
-		<div class="inner">
-			<Entry img="/egelleeft.png" />
-			<Entry img="/egelleeft.png" />
-			<Entry img="/egelleeft.png" />
-			<Entry img="/egelleeft.png" />
+	<div class="2xl:w-3/5 sm:w-4/5 my-12">
+		<h2 class="w-full text-4xl font-semibold mb-4 border-b border-gray-300 pb-3">Tijdlijn</h2>
+		<div
+			class="timeline overflow-y-auto flex flex-col justify-between w-full bg-gray-100 rounded-lg shadow-md"
+		>
+			<div class="inner">
+				<Entry img="/egelleeft.png" />
+				<Entry img="/egelleeft.png" />
+				<Entry img="/egelleeft.png" />
+				<Entry img="/egelleeft.png" />
+			</div>
 		</div>
 	</div>
+	<div class="flex flex-col items-center relative w-full my-12 bg-gray-200">
+		<Line color="bg-gray-300" />
+		<div class="background1 absolute -top-96 h-96 w-full -z-40" />
 
-	<Line />
+		<div class="2xl:w-3/5 sm:w-4/5">
+			<article class="w-full mb-12">
+				<h1 class="text-4xl font-semibold mb-4 border-b border-gray-300 pb-3">
+					Bijdragen aan de tijdlijn
+				</h1>
+				<p class="text-lg">
+					Heb je interesse in het helpen bij het opschrijven van belangrijke gebeurtenissen binnen
+					Egel? Stuur hier dan de informatie over de gebeurtenis en potentieel een afbeelding op
+					naar de Egel Historians! Alle inzendingen worden eerst gecontroleerd voordat ze toegevoegd
+					worden aan de tijdbalk.
+					<br />
+					Voor vragen over de richtlijnen om te volgen bij je inzending, kun je terecht in de Discord
+					server.
+				</p>
+			</article>
+			<form class="flex justify-between">
+				<div class="w-1/3 mr-8">
+					<label for="title" class="block text-lg font-semibold mb-2">Titel</label>
+					<input
+						type="text"
+						name="title"
+						id="title"
+						class="w-full rounded-lg border outline-none border-gray-300 p-2 mb-4 focus:border-purple-500 transition"
+					/>
+					<label for="title" class="block text-lg font-semibold mb-2">Datum</label>
+					<input
+						type="date"
+						name="title"
+						id="title"
+						class="w-full rounded-lg border outline-none border-gray-300 p-2 mb-4 focus:border-purple-500 transition"
+					/>
+					<label for="description" class="block text-lg font-semibold mb-2">Beschrijving</label>
+					<textarea
+						name="description"
+						id="description"
+						class="w-full  rounded-lg border outline-none border-gray-300 p-2 mb-4 focus:border-purple-500 transition"
+					/>
+					<button
+						type="submit"
+						class="w-full rounded-lg border outline-none border-gray-300 p-2 mb-2 text-white bg-purple-400 hover:bg-purple-500 transition"
+					>
+						Verstuur
+					</button>
+				</div>
+
+				<ImgButton />
+			</form>
+		</div>
+
+		<div class="background2 absolute -bottom-96 h-96 w-full -z-40" />
+		<Line color="bg-gray-300" />
+	</div>
+
 	<TeamCard
 		title="Egel Historians"
 		title1="MeneerValk"
@@ -94,5 +153,13 @@
 		&::-webkit-scrollbar-thumb:hover {
 			background: rgba(255, 255, 255, 0.4);
 		}
+	}
+
+	.background1 {
+		background-image: -webkit-linear-gradient(95deg, rgb(229 231 235) 50%, white 50.3%);
+	}
+
+	.background2 {
+		background-image: -webkit-linear-gradient(95deg, white 50%, rgb(229 231 235) 50.3%);
 	}
 </style>
