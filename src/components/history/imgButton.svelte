4f2cdb68a422506cfc9e9ffa90d3a9eb14ff94f9
MrValk <n.s.peters05@gmail.com>
2022-10-05 17:16:51 +0200

    Fixed imgButton code. I forgot to migrate it to the new cleaned up code.

--- src/components/history/imgButton.svelte
+++ src/components/history/imgButton.svelte
@@ -1,51 +1,19 @@
 <script lang="ts">
-	import { browser } from '$app/environment';
 	import Fa from 'svelte-fa';
 	import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
 	import { setImg, imgStore } from '../../stores/imgStore';
 
-	// @ts-ignore
-	import cropImage from '../../scripts/cropImage';
+	import getImgUrl from '../../scripts/cropImage';
 
 	async function updateImgPreview() {
-		if (!browser) return;
 		if (!imgInput.files || !imgInput.files[0]) return;
 
-		const reader = new FileReader();
-
-		reader.readAsDataURL(imgInput.files[0]);
-
-		reader.onload = function (event) {
-			const imgElement = document.createElement('img');
-			if (!imgElement || !event.target || !event.target.result) return;
-
-			imgElement.src = event.target.result as string;
-
-			imgElement.onload = function (e) {
-				const img = e.target as CanvasImageSource;
-				if (
-					!e.target ||
-					!img.width ||
-					!img.height ||
-					typeof img.width !== 'number' ||
-					typeof img.height !== 'number'
-				)
-					return;
-
-				const canvas = document.createElement('canvas');
-				canvas.width = 384;
-				canvas.height = 216;
-
-				const ctx = canvas.getContext('2d');
-				if (!ctx) return;
-
-				cropImage(ctx, img, 0, 0, canvas.width, canvas.height);
-
-				// console.log(imgUrl);
-				setImg(ctx.canvas.toDataURL('image/jpg'));
-			};
-		};
+		try {
+			setImg(await getImgUrl(imgInput.files[0], 128, 128));
+		} catch (er) {
+			console.error(er);
+		}
 	}
 
 	let imgInput: HTMLInputElement;
