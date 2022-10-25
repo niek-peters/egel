426d8b3b53e0190747d0574a3246cf106707f27c
MrValk <n.s.peters05@gmail.com>
2022-10-25 13:36:00 +0200

    Added a dynamic dummy page for the different news items.

---
+++ src/routes/(home)/nieuws/[uid]/+page.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import { Circle } from 'svelte-loading-spinners/src';
+
+	import { page } from '$app/stores';
+
+	let loaded = false;
+	let loading = false;
+
+	$: $page.params.uid, load();
+
+	async function load() {
+		if (loading) return;
+		loading = true;
+
+		loaded = true;
+		loading = false;
+	}
+</script>
+
+{#if loading}
+	<div class="wrapper flex flex-col items-center justify-center">
+		<Circle color="#9CA3AF" />
+	</div>
+{:else if loaded}
+	Dit is artikel {$page.params.uid}
+{:else}
+	<div class="wrapper flex flex-col items-center justify-center gap-6">
+		<h2 class="text-4xl font-semibold">Artikel niet gevonden</h2>
+		<p>Dit artikel bestaat niet of is verwijderd.</p>
+	</div>
+{/if}
